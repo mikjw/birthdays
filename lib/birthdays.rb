eb1e7540a3b736b1fbac987a8d038e145552404f
Mike <mjwalshe@gmail.com>
2019-09-01 09:59:37 +0100

    Inject Time class

--- lib/birthdays.rb
+++ lib/birthdays.rb
@@ -1,9 +1,12 @@
 class Birthdays
+  
+  attr_reader :time
+  
+  def initialize(time = Time.new)
+    @time = time
+  end 
 
   def print_list
     @list = {:Jon => [5,5], :Sally => [6,5], :Mick => [1,8]}
     @list.each {|k, v| puts "#{k}: #{v[0]}/#{v[1]}" }
   end
-
-
-
